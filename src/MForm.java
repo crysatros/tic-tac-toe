245ab7df8be4468b2957ebf74f2bcf062f4cc76d
crysatros <44719664+crysatros@users.noreply.github.com>
2019-05-16 23:12:47 +0700

    Add files via upload

---
+++ src/MForm.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MForm extends JFrame {
+    public MForm()
+    {
+        setTitle("Welcome to Tic-Tac-Toe game!");
+        setBounds(300, 300, 455, 525);
+        setResizable(false);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        final GameField gameField = GameField.getInstance();
+        JPanel buttonPanel = new JPanel(new GridLayout());
+        add(gameField, BorderLayout.CENTER);
+        add(buttonPanel, BorderLayout.SOUTH);
+        final JButton Start = new JButton("New game!");
+        final JButton End = new JButton("Close game...");
+        buttonPanel.add(Start);
+        buttonPanel.add(End);
+        End.addActionListener(new ActionListener() {
+            @Override
+
+            public void actionPerformed(ActionEvent e)
+            {
+                System.exit(0);
+            }
+        });
+        Start.addActionListener(new ActionListener()
+        {
+            @Override
+
+            public void actionPerformed(ActionEvent e)
+            {
+                System.out.println(Start.getText());
+                gameField.startNewGame();
+            }
+        });
+        setVisible(true);
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,23 @@
+public class Player extends Gamer{
+    GameField gameField;
+    int isShotReady = 1;
+    public Player(String sign)
+    {
+        this.sign = sign;
+    }
+    boolean shoot(int x, int y)
+    {
+        gameField = GameField.getInstance();
+        if (!gameField.isCellBusy(x, y))
+        {
+            gameField.cell[x][y] = sign;
+            return true;
+        }
+        return false;
+    }
+    boolean win()
+    {
+        gameField = GameField.getInstance();
+        return gameField.checkWin(this.sign);
+    }
+}
